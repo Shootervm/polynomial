0aff3c17ee9fdce6b89b5f445ddbbdaa5cdce72e
Vojtech Mašek <shooter.vm@gmail.com>
2016-04-15 03:59:34 +0200

    added __add__ operator corrected __str__ and __call__

--- Polynomial.py
+++ Polynomial.py
@@ -22,8 +22,9 @@ class Polynomial(object):
         self.deg = len(self.coeffs)  # is used as range limit, if representing degree `self.deg - 1` needs to be used
 
     def __str__(self):
-        # Iterate coefficients into a list of non-zero terms
-        terms = ['{0:+d}'.format(self.coeffs[k]) + ('x^' + str(k) if k > 1 else self.x[k])
+        # Create list of terms by iterating coefficients and processing non-zero terms to correctly formatted items
+        terms = [('{0:+d}'.format(self.coeffs[k]) if self.coeffs[k] > 1 or k == 0 else '') +
+                 ('x^' + str(k) if k > 1 else self.x[k])
                  for k in range(0, self.deg) if self.coeffs[k] != 0]
 
         if len(terms) == 0:
@@ -35,13 +36,27 @@ class Polynomial(object):
         """Evaluates the Polynomial at value of x"""
         return reduce(add, [self.coeffs[i] * (x ** i) for i in range(self.deg)])
 
-    def at_value(self, x, y=0):
-        return self.__call__(x) if y == 0 else self.__call__(y) - self.__call__(x)
+    def at_value(self, x, y=None):
+
+        return self.__call__(x) if y is None else self.__call__(y) - self.__call__(x)
+
+    def __add__(self, other):
+        """Adds two Polynomials and will overload + operator when called."""
+
+        terms = [0] * max(self.deg, other.deg)
+        for i in range(self.deg):
+            terms[i] = self.coeffs[i]
+        for i in range(other.deg):
+            terms[i] = terms[i] + other.coeffs[i]
+
+        return Polynomial(terms)
 
 
 def start():
-    # pol1 = Polynomial([1, -3, 0, 2])
-    # pol2 = Polynomial([-1, 3])
+    pol1 = Polynomial([1, -3, 0, 2])
+    pol3 = Polynomial(1, -3, 0, 2)
+    pol2 = Polynomial([1, -3])
+    print(Polynomial(x0=1, x1=1))
 
     # print(pol1)
     # print(pol1.at_value(2))
@@ -50,7 +65,7 @@ def start():
 
     # print(Polynomial(1, -3, 0, 2))
     # print(Polynomial(x0=1, x3=2, x1=-3))
-    print(Polynomial(x0=5, x3=2, x4=4, x5=4, x1=-3))
+    # print(Polynomial(x0=5, x3=2, x4=4, x5=4, x1=-3))
     # "2x^3 - 3x + 1"
 
     # print(pol2)
