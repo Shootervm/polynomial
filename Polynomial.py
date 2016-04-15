a4f253b38dd3e9463ae56081f5112e63b4061197
Vojtech Mašek <shooter.vm@gmail.com>
2016-04-15 12:44:40 +0200

    added derivative() method refactored code

--- Polynomial.py
+++ Polynomial.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import re
 from sys import stderr
 from operator import add
@@ -12,13 +13,11 @@ class Polynomial(object):
     x = ['', 'x']
 
     def __init__(self, *args, **kwargs):
-        print(args)
-        print(kwargs)
-        if args and isinstance(args[0], list):  # Polynomial([1,-3,0,2])
+        if args and isinstance(args[0], list):  # Polynomial([#list#])
             self.coeffs = args[0]
-        elif args:  # Polynomial(1,-3,0,2)
+        elif args:  # Polynomial(#args#)
             self.coeffs = args
-        else:  # Polynomial(x0=1,x3=2­,x1=-3)
+        else:  # Polynomial(#named args#)
             self.coeffs = [kwargs.get(x, 0) for x in ['x' + str(i) for i in range(len(kwargs) + 1)]]
 
         self.deg = len(self.coeffs)  # is used as range limit, if representing degree `self.deg - 1` needs to be used
@@ -55,58 +54,33 @@ class Polynomial(object):
     def __mul__(self, other):
         """Multiplies two Polynomials and will overload * operator when called."""
 
-        n = self.deg + other.deg  # Degree of product
-        prod_coeffs = [0] * (self.deg + other.deg + 1)  # Initalize coefficient list of product
-        # Compute Cauchy product
+        prod_coeffs = [0] * (self.deg + other.deg + 1)  # Initialize coefficient list of product
         for i in range(0, self.deg):
             for j in range(0, other.deg):
                 prod_coeffs[i + j] += self.coeffs[i] * other.coeffs[j]
 
         return Polynomial(prod_coeffs)
 
-    def __pow__(self, power, modulo=None):
+    def __pow__(self, power):
         if power < 0:
             raise ValueError("Power must be a non-negative integer.")
-        # TODO: toto tu je dojebane
-        elif self.coeffs == 0:  # TODO: toto tu je dojebane
-            raise ValueError("Polynomial is 0")
         elif power == 0:
             return Polynomial(1)
         elif power == 1:
             return self
         else:
-            # This can be made more efficient by using powers of two
-            # in the usual way.
             result = self
             for i in range(2, power + 1):
                 result = result * self
 
             return result
 
+    def derivative(self):
+        return Polynomial([self.coeffs[d] * d for d in range(1, len(self.coeffs))])
+
 
 def start():
-    # pol1 = Polynomial([1, -3, 0, 2])
-    # pol3 = Polynomial(1, -3, 0, 2)
-    # pol2 = Polynomial([1, -3])
-
-    # print(pol1 ** 2)
-    # print(Polynomial([-1, 1]) ** 2)
-    print('***********************************')
-    #print(Polynomial(-1, 1) ** 2)
-    print(Polynomial(0) ** 0)
-
-    # print(pol1)
-    # print(pol1.at_value(2))
-    # print(pol1.at_value(3))
-    # print(pol1.at_value(2, 3))
-
-    # print(Polynomial(1, -3, 0, 2))
-    # print(Polynomial(x0=1, x3=2, x1=-3))
-    # print(Polynomial(x0=5, x3=2, x4=4, x5=4, x1=-3))
-    # "2x^3 - 3x + 1"
-
-    # print(pol2)
-    # print(pol1 + pol2)
+    pass
 
 
 if __name__ == "__main__":
